7c20698d0d574ae71de12313221cb6aebb8641d2
Sujal <Sujalgdgoc@gmail.com>
2026-08-20 12:15:24 +0530

    fixed wsgi

--- vigil/wsgi.py
+++ vigil/wsgi.py
@@ -6,10 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')  # Replace with your project folder name
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vigil.settings')
 
 application = get_wsgi_application()
